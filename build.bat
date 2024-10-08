5a11c50088ad9b2ac411001934599194405e3543
Abel <56084910+Abelkrijgtalles@users.noreply.github.com>
2024-10-08 21:59:04 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,16 +5,10 @@ cd /d "%~dp0"
 
 set "versions="
 
-echo Building the following versions:
-
 for %%f in (props\*.properties) do (
     set "filename=%%~nf"
     set "versions=!versions! %%~nf"
-    echo %%~nf
 )
-
-echo.
-echo Beginning to build...
 
 for %%v in (%versions%) do (
     call gradlew -q -Pminecraft=%%v build
